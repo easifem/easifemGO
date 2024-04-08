4e82edd8b5f9ade3b98f1ba26a8ff770527b415c
Vikas Sharma <vickysharma0812@gmail.com>
2024-04-08 12:52:49 +0900

    adding clean command
    Adding clean coomand

---
+++ cmd/clean.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// cleanCmd represents the clean command
+// examples
+// easifem clean
+var cleanCmd = &cobra.Command{
+	Use:   "clean pkgname [flags]",
+	Short: "remove build and install files of a pkg",
+	Long:  easifem_clean_intro,
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(easifem_banner)
+		pwd, err := os.Getwd()
+		if err != nil {
+			log.Fatalln("[err] :: clean.go | os.Getwd() ➡️ ", err)
+		}
+		for _, pkg := range args {
+			if pkg == "extpkgs" {
+				pkgs := get_ext_pkgs()
+				for _, p := range pkgs {
+					cleanPkgs(p, pwd)
+				}
+			} else {
+				cleanPkgs(pkg, pwd)
+			}
+		}
+	},
+}
+
+// clean a package
+func cleanPkgs(pkg, pwd string) {
+	// source_dir := install_get_source_dir(pkg)
+	build_dir := install_get_build_dir(pkg)
+	install_dir := install_get_install_dir(pkg)
+
+	if pwd == build_dir {
+		log.Println("[log] :: clean.go() | build_dir is same as current dir")
+		log.Println("[log] :: clean.go() | cannot clean current directory for pkg: " + pkg)
+	}
+
+	if err := os.RemoveAll(build_dir); err != nil {
+		log.Println("[log] :: clean.go() | could not clean pkg: " + pkg)
+	}
+
+	if pwd == install_dir {
+		log.Println("[log] :: clean.go() | install_dir is same as current dir")
+		log.Println("[log] :: clean.go() | cannot clean current directory for pkg: " + pkg)
+	}
+
+	if err := os.RemoveAll(install_dir); err != nil {
+		log.Println("[log] :: clean.go() | could not clean pkg: " + pkg)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(cleanCmd)
+}
